6f3cc56d5c2ab340d74243c8e54c718500695c57
Victor9578 <2410512860@qq.com>
2023-11-26 20:57:26 +0800

    4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json, requests, random
+import json, requests, random, ctypes, os
 from PIL import Image
 
 url_googearth: str = "https://earthview.withgoogle.com/_api/photos.json"
@@ -11,26 +11,26 @@ def img_fromgoog(url: str) -> str:
 
 
 def save_jpg() -> str:
-    name_img: str = json.loads(img_fromgoog(url_googearth))[int(random.random() * 2000)]["slug"]
-    url_imgjson2: str = (
-        url_imgjson1
-        + name_img
-        + ".json"
-    )
+    name_img: str = json.loads(img_fromgoog(url_googearth))[
+        int(random.random() * 2000)
+    ]["slug"]
+    url_imgjson2: str = url_imgjson1 + name_img + ".json"
     url_img: str = json.loads(img_fromgoog(url_imgjson2))["photoUrl"]
     with open("./1.jpg", "wb") as f:
         f.write(img_fromgoog(url_img))
     return name_img
 
+
 def img_convert():
-    Image.open("./1.jpg").convert('L').save("2.jpg")
+    Image.open("./1.jpg").convert("L").save("./2.jpg")
+    return os.path.abspath("2.jpg")
 
 
-
-def __main__():
-    save_jpg()
-    img_convert()
+def setwallpaper(location_img):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, location_img, 1)
 
 
-
-__main__()
+if __name__ == "__main__":
+    # save_jpg()
+    print(img_convert())
+    setwallpaper(img_convert())
