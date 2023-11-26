a546c173acb484d6058a164916865ed7024f711e
Victor9578 <2410512860@qq.com>
2023-11-26 20:23:57 +0800

    3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json, requests, random
+from PIL import Image
 
 url_googearth: str = "https://earthview.withgoogle.com/_api/photos.json"
-url_imgjson: str = "https://earthview.withgoogle.com/_api/"
+url_imgjson1: str = "https://earthview.withgoogle.com/_api/"
 
 
 def img_fromgoog(url: str) -> str:
@@ -9,18 +10,27 @@ def img_fromgoog(url: str) -> str:
     return respone
 
 
-def main():
-    photo_json: str = img_fromgoog(url_googearth)
-    url_photo = (
-        url_imgjson
-        + json.loads(photo_json)[int(random.random() * 2000)]["slug"]
+def save_jpg() -> str:
+    name_img: str = json.loads(img_fromgoog(url_googearth))[int(random.random() * 2000)]["slug"]
+    url_imgjson2: str = (
+        url_imgjson1
+        + name_img
         + ".json"
     )
-    urlending = json.loads(img_fromgoog(url_photo))["photoUrl"]
-    print(urlending)
-    print(type(img_fromgoog(urlending)))
+    url_img: str = json.loads(img_fromgoog(url_imgjson2))["photoUrl"]
     with open("./1.jpg", "wb") as f:
-        f.write(img_fromgoog(urlending))
+        f.write(img_fromgoog(url_img))
+    return name_img
+
+def img_convert():
+    Image.open("./1.jpg").convert('L').save("2.jpg")
 
 
-main()
+
+def __main__():
+    save_jpg()
+    img_convert()
+
+
+
+__main__()
